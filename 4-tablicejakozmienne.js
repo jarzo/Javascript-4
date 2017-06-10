6a1bb5a7a8e3281ad6da9474aade1753c2cf4a24
Michał Jarząbek <nebraska994@gmail.com>
2017-06-10 11:20:22 +0200

    tablice Array

---
+++ 4-tablicejakozmienne.js
@@ -0,0 +1,26 @@
+'use strict'
+
+
+
+/*Tablice - Arrays*/
+
+
+
+var imiona = new Array();
+var imionaStudentow = new Array(9);
+var imionaChlopakow = new Array('Mateusz','Michał','Krzysiek','Sergio');
+
+imionaStudentow[2] = "Przemek";
+
+console.log(imiona);
+console.log(imionaStudentow);
+console.log(imionaChlopakow);
+
+var wiek = [];
+var wiekStudentow = [20, 25, 28];
+
+console.log(wiek);
+console.log(wiekStudentow);
+
+/*var imiona = Array() to jest to samo co var imiona = []*/
+
